3cf225ee982104e096767f17219a0d28c1e3c411
EugenKoptikov <evgenkoptikov100500@gmail.com>
2022-07-01 17:55:13 +0300

    Exercise 10 created!

---
+++ Exercise 10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Программа для нахождения и вывода второй цифры в трехзначом числе!");
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+
+if (number.Length == 3)
+{
+    char secondChar = number[1];
+    Console.WriteLine("Искомая цифра " + secondChar);
+} 
+if (number.Length <3 || number.Length > 3) {
+    Console.WriteLine("Неверно введено число!");
+}
