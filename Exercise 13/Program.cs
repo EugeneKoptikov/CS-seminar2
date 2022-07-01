2cce952215c9515357d63da00547b1405eda7cf0
EugenKoptikov <evgenkoptikov100500@gmail.com>
2022-07-01 17:53:46 +0300

    Exercise 13 created!

--- Exercise 13/Program.cs
+++ Exercise 13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Программа для нахождения и вывода второй цифры в трехзначом числе!");
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+
+if (number.Length >= 3)
+{
+    char thirdChar = number[2];
+    Console.WriteLine("Искомая цифра " + thirdChar);
+} 
+if (number.Length < 3 ) {
+    Console.WriteLine("Неверно введено число!");
+}
